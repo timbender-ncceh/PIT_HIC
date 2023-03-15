bacabbe2b4f051319df17c6f8232a9872f3faa4e
timbender-ncceh <105810134+timbender-ncceh@users.noreply.github.com>
2023-03-15 15:41:20 -0400

    Update pit_youth_veteran_hh_type_logic.R
    working but not finished yet. i don't think there are any bugs in code as-writ.

--- pit_youth_veteran_hh_type_logic.R
+++ pit_youth_veteran_hh_type_logic.R
@@ -18,22 +18,22 @@ rm(list=ls())
 cat('\f')
 gc()
 
-
-
 # note:  veterans are not alaways hoh so don't encode logic to account for hoh
 # necessarily
 
 # funs----
 
-get_youth.hh.info <- function(hh_pid_ages.v, age.hoh = NULL,
-                              with_parent.child = F){
+hh_pid_ages.v <- c(24,2,32)
+age.hoh <- 32
+
+get_youth.hh.info <- function(hh_pid_ages.v, age.hoh = NULL){
   require(dplyr)
   require(data.table)
   require(glue)
   require(crayon)
   # tidy
-  hh_pid_ages.v <- hh_pid_ages.v %>%
-    .[order(.)]
+  # hh_pid_ages.v <- hh_pid_ages.v %>%
+  #   .[order(.)]
   
   # error checking---- 
   
@@ -74,8 +74,7 @@ get_youth.hh.info <- function(hh_pid_ages.v, age.hoh = NULL,
   youth_hh_type <- NA
   
   # is_youth_hh
-  if(min(hh_pid_ages.v) <= 24 & 
-     max(hh_pid_ages.v) <= 24) {
+  if(all(hh_pid_ages.v <= 24)) { # this number at 24 is correct for identifying the hh type
     is_youth_hh <- T
   }else{
     is_youth_hh <- F
@@ -112,7 +111,47 @@ get_youth.hh.info <- function(hh_pid_ages.v, age.hoh = NULL,
     }
   }
   
-  # SUBTYPE
+  # HH Age Group
+  
+  # step 1: age grouping 
+  # (18-24)
+  if(all(between(hh_pid_ages.v, 18, 24))){
+    ag18.24 <- T
+  }else{
+    ag18.24 <- F
+  }
+  # (<18 & 18-24 in one hh)
+  if(any(hh_pid_ages.v < 18) & 
+     any(between(hh_pid_ages.v, 18, 24)) & 
+     all(hh_pid_ages.v <= 24)){
+    ag18and18.24 <- T
+  }else{
+    ag18and18.24 <- F
+  }
+  # (<18) 
+  if(all(hh_pid_ages.v < 18)){
+    agU18 <- T
+  }else{
+    agU18 <- F
+  }
+  # (>=25)
+  if(any(hh_pid_ages.v > 24)){
+    ag25O <- T
+  }else{
+    ag25O <- F
+  }
+  
+  out.ages <- data.frame(variable = c("(18-24)", 
+                                      "(<18 & 18-24 in one hh)", 
+                                      "(<18)", 
+                                      "(>=25)"), 
+                         value    = c(ag18.24, 
+                                      ag18and18.24, 
+                                      agU18, 
+                                      ag25O)) %>% 
+    as_tibble()
+  
+  # HH subtype----
   c("Unaccompanied youth (18-24)",
     "Parenting youth (18-24)",
     "Children of parenting youth",
@@ -123,73 +162,94 @@ get_youth.hh.info <- function(hh_pid_ages.v, age.hoh = NULL,
     "Other people (<18)", 
     "Other people (18-24)")
   
-  # step 1: age grouping 
-  if(all(between(x = hh_pid_ages.v, 
-                 lower = 18, 
-                 upper = 24))){
-    # step 2a: hh size = 1 AND parent-child
-    if(length(hh_pid_ages.v) > 1 & 
-       with_parent.child == T){
-      youth_hh_subtype <- c("<ERROR - this would be a hh with a parent and child, but everybody is 18-24>")
-      # else hh size != 1 AND not parent-child
+  temp.out.age <- out.ages$variable[out.ages$value]
+ 
+  # error check----
+  if(length(temp.out.age) > 1){
+    stop("ERROR: var 'temp.out.age' should be length of 1")
+  }
+  # unaccompanied youth
+  # if is_youth household
+  if(is_youth_hh){
+    if(youth_hh_type == "Households with at least 1 adult and 1 child"){
+      youth_hh_subtype <- c("Parenting youth", "Children of parenting youth")
     }else{
-      youth_hh_subtype <- "Unaccompanied youth (18-24)"
+      youth_hh_subtype <- NA # this needs to eventually go away.  it's a catch-all for missing logic
     }
     
-  }
-  if(all(hh_pid_ages.v <= 25)){
-    youth_hh_subtype <- "Unaccompanied youth (<18 & 18-24 in one hh)"
-  }
-  if(all(hh_pid_ages.v < 18)){
-    youth_hh_subtype <- "Unaccompanied youth (<18)"
+    
+    
+  }else{
+    # it's not is_youth and thus, is the "other people"
+    if(temp.out.age %in%  c("(<18)", 
+                            "(18-24)", 
+                            "(<18 & 18-24 in one hh)")){
+      youth_hh_subtype <- "<ERROR - not possible for house to be not_youth_hh but have all people under 25>"
+    }
+    if(temp.out.age == ("(>=25)")){
+      youth_hh_subtype <- "Other people (>=25)"
+    }
+    
   }
   
   
   # generate output
-  out <- list(is.youth.hh = is_youth_hh, 
-              youth.hh.type = youth_hh_type, 
+  out <- list(hh.input.summary = data.frame(hh_size = length(hh_pid_ages.v), 
+                                            hoh_age = age.hoh, 
+                                            min_age = min(hh_pid_ages.v), 
+                                            max_age = max(hh_pid_ages.v)),
+              is.youth.hh      = is_youth_hh, 
+              youth.hh.type    = youth_hh_type, 
               youth.hh.subtype = youth_hh_subtype, 
+              youth.hh.agetype = out.ages$variable[out.ages$value],
               poss.flags       = poss_flags)
   
   # print inputs to help with debugging
-  cat(inverse(glue("AGES:\t{paste(hh_pid_ages.v,sep=\" \", collapse = \" \")}\nPARENT:\t{with_parent.child}\n\n")))
-  
+  #cat(inverse(glue("AGES:\t{paste(hh_pid_ages.v,sep=\" \", collapse = \" \")}\nPARENT:\t{with_parent.child}\n\n")))
+  cat(inverse(glue("AGES:\t{paste(hh_pid_ages.v,sep=\" \", collapse = \" \")}\nAGEHoH:\t{age.hoh}\n\n")))
   return(out)
   
 }
 
-
+args(get_youth.hh.info)
+get_youth.hh.info(c(1,17,19), 17)
 
 
 # testing----
 
 # generate random household of ages and size
 for(i in 1:10000){
-  rm(hohage)
+  
   some.ages <- sample(1:45, size = sample(1:3, size = 1), replace = T)
-  if(length(some.ages) > 1){
-    hohage <- max(some.ages)
+  
+  if(sum(some.ages>17) > 1){
+    hohage <- sample(some.ages[some.ages > 17], size = 1)
   }else{
-    hohage <- NULL
+    hohage <- max(some.ages)
   }
+  
+  # if(length(some.ages) > 1){
+  #   hohage <- max(some.ages)
+  # }else{
+  #   hohage <- NULL
+  # }
   
   # decide (randomly, but with some logic) whether there is a parent-child
   # relationship in household
-  is.parentchild <- ifelse(length(some.ages) > 1 & any(some.ages > 17) & any(some.ages <= 17), sample(c(T,F), size = 1), F)
+  #is.parentchild <- ifelse(length(some.ages) > 1 & any(some.ages > 17) & any(some.ages <= 17), sample(c(T,F), size = 1), F)
   
   # run script
   test.out <- get_youth.hh.info(hh_pid_ages.v = some.ages, 
-                                age.hoh = hohage,
-                                with_parent.child = is.parentchild)
+                                age.hoh = hohage)
   
   if(is.na(test.out$youth.hh.type) | is.na(test.out$youth.hh.subtype)){
     print(i);get_youth.hh.info(hh_pid_ages.v = some.ages, 
-                               age.hoh =  hohage,
-                                         with_parent.child = is.parentchild)
+                               age.hoh =  hohage)
     break
   }
 }
 
+get_youth.hh.info(some.ages, hohage)
 
 # EXAMPLE 1:
 
