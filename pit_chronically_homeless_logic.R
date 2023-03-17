3097fec8b813cb156e1288a9a9a2db6bc3356bb7
timbender-ncceh <105810134+timbender-ncceh@users.noreply.github.com>
2023-03-17 10:59:25 -0400

    Create pit_chronically_homeless_logic.R
    initial commit

---
+++ pit_chronically_homeless_logic.R
@@ -0,0 +1,8 @@
+# chronically homeless logic
+
+
+# see: 
+
+"https://files.hudexchange.info/resources/documents/HMIS-Standard-Reporting-Terminology-Glossary.pdf"
+
+# section on chronic homelessness.  has logic flow
