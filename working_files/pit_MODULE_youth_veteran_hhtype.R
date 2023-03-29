5f34a866da1e709b3620164e97ad6b516686e32d
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-03-29 16:22:21 -0400

    Update pit_MODULE_youth_veteran_hhtype.R
    major changes to make this work as a source() file

--- working_files/pit_MODULE_youth_veteran_hhtype.R
+++ working_files/pit_MODULE_youth_veteran_hhtype.R
@@ -1,62 +1,25 @@
-# simulate youth_ veteran_ households scenarios
-# Libraries----
-library(dplyr)
-library(readr)
-library(glue)
-library(lubridate)
-library(data.table)
-library(devtools)
-library(openxlsx)
-library(ggplot2)
+# MODULE to produce youth_ veteran_household_type
 
-rm(list=ls())
-cat('\f')
-gc()
-csv.file.dir <- "C:/Users/TimBender/Documents/R/ncceh/projects/pit_survey/January_2023/real_data"
-setwd(csv.file.dir)
-
-# Functions----
-#devtools::source_url(url = "https://raw.githubusercontent.com/timbender-ncceh/PIT_HIC/main/pit_survey_calculations.R?raw=TRUE")
-# updated to reflect dev branch ^^^
-devtools::source_url(url = "https://raw.githubusercontent.com/timbender-ncceh/PIT_HIC/dev-PIT_week_12/pit_survey_calculations.R?raw=TRUE")
-
-rm(list=ls()[!ls() %in% c("get_youth.hh.info", 
-                          "calc_age", 
-                          "fun_rel2hoh", 
-                          "fun_1.8_def", 
-                          "make_abbr", 
-                          "hh_age.unknown", 
-                          "hh_wal1a1c", 
-                          "hh_wo.c", 
-                          "hh_w.o.C", 
-                          "py_u18", 
-                          "py_18.24", 
-                          "uy", 
-                          "hh_vet", 
-                          "hh_youth")])
-
-# data----
-pit.night    <- ymd(20230125)
-a.enrollment <- read_csv("Enrollment.csv")
-a.client     <- read_csv("Client.csv")
-
+temp.enrollment <- read_csv("Enrollment.csv")
+temp.client     <- read_csv("Client.csv")
 
 # tidy----
-a.client$age                       <- calc_age(a.client$DOB, 
+temp.client$age                       <- calc_age(temp.client$DOB, 
                                                decimal.month = F,
                                                age_on_date = pit.night)
-a.client$VeteranStatus_def         <- unlist(lapply(X = a.client$VeteranStatus, 
+temp.client$VeteranStatus_def         <- unlist(lapply(X = temp.client$VeteranStatus, 
                                                     FUN = fun_1.8_def))
-a.enrollment$RelationshipToHoH_def <- unlist(lapply(X = a.enrollment$RelationshipToHoH, 
+temp.enrollment$RelationshipToHoH_def <- unlist(lapply(X = temp.enrollment$RelationshipToHoH, 
                                                     FUN = fun_rel2hoh))
 
 
 # REAL DATA----
-real.df <- left_join(a.enrollment[,c("HouseholdID", "PersonalID", "EnrollmentID", 
+real.df <- left_join(temp.enrollment[,c("HouseholdID", "PersonalID", "EnrollmentID", 
                                      "RelationshipToHoH_def")], 
-                     a.client[,c("PersonalID", "age", 
+                     temp.client[,c("PersonalID", "age", 
                                  "VeteranStatus_def")]) 
 
+rm(temp.client,temp.enrollment)
 
 real.df$hh1_age.unknown <- NA
 real.df$hh1_vet         <- NA
@@ -67,7 +30,6 @@ real.df$hh1_youth       <- NA
 real.df$py1_18.24       <- NA
 real.df$py1_u18         <- NA
 real.df$uy1             <- NA
-
 
 for(i in unique(real.df$HouseholdID)){
  #print(i)
@@ -86,115 +48,36 @@ for(i in unique(real.df$HouseholdID)){
                                                             rel2hohs = real.df$RelationshipToHoH_def[real.df$HouseholdID == i])
   }
 }
+rm(i)
 
-colnames(real.df)
-unique(real.df$RelationshipToHoH_def)
-
-real.df.out <- real.df %>%
-  group_by(HouseholdID,
-           hh1_youth, 
-           hh1_vet, 
-           hh1_age.unknown,       
-           hh1_w.o.C, 
-           hh1_wal1a1c, 
-           hh1_wo.c, 
-           py1_u18, 
-           py1_18.24, 
-           uy1) %>%
-  summarise(hh_size = n_distinct(PersonalID), 
-            #n_hhid = n_distinct(HouseholdID), 
-            n_vets  = sum(VeteranStatus_def == "Yes", na.rm = T),
-            n_HoH   = sum(RelationshipToHoH_def == "Self (head of household)", na.rm = T),
-            n_ChildOfHoH = sum(RelationshipToHoH_def == "Head of household’s Child", na.rm = T),
-            min_age = min(age), 
-            max_age = max(age),
-            max_known.age = max(age,na.rm = T),
-            nAge_0to17  = sum(age < 18, na.rm = T), 
-            nAge_18to24 = sum(age >= 18 & age <= 24, na.rm = T), 
-            nAge_25up   = sum(age >= 25, na.rm = T)) %>%
-  ungroup() %>% 
-  mutate(., 
+real.df <- real.df %>%
+  mutate(.,
          hh1_youth       = ifelse(hh1_youth, "YOUTH", ""),
          hh1_age.unknown = ifelse(hh1_age.unknown, "AGE_UNKNOWN", ""),
          hh1_vet         = ifelse(hh1_vet == "Yes", "VETERAN", ""),
-         hh1_w.o.C       = ifelse(hh1_w.o.C, "hh_with_only_children", ""), 
-         hh1_wal1a1c     = ifelse(hh1_wal1a1c, "hh_with_1adult1child", ""), 
-         hh1_wo.c        = ifelse(hh1_wo.c, "hh_without_children", ""), 
-         py1_u18         = ifelse(py1_u18, "py_under18", ""), 
-         py1_18.24       = ifelse(py1_18.24, "py_18to24", ""), 
+         hh1_w.o.C       = ifelse(hh1_w.o.C, "hh_with_only_children", ""),
+         hh1_wal1a1c     = ifelse(hh1_wal1a1c, "hh_with_1adult1child", ""),
+         hh1_wo.c        = ifelse(hh1_wo.c, "hh_without_children", ""),
+         py1_u18         = ifelse(py1_u18, "py_under18", ""),
+         py1_18.24       = ifelse(py1_18.24, "py_18to24", ""),
          uy1             = ifelse(uy1, "unaccomp_youth", "")) %>%
-  mutate(., 
-         out_hh_type = paste(hh1_youth, 
-                             hh1_age.unknown, 
-                             hh1_vet, 
-                             hh1_w.o.C, 
-                             hh1_wal1a1c, 
-                             hh1_wo.c, 
-                             py1_u18, 
-                             py1_18.24, 
-                             uy1, 
+  mutate(.,
+         out_hh_type = paste(hh1_youth,
+                             hh1_age.unknown,
+                             hh1_vet,
+                             hh1_w.o.C,
+                             hh1_wal1a1c,
+                             hh1_wo.c,
+                             py1_u18,
+                             py1_18.24,
+                             uy1,
                              sep = "-")) %>%
-  mutate(., 
-         out_hh_type = gsub(pattern = "^-{1,}|-{1,}$", "", out_hh_type), 
+  mutate(.,
+         out_hh_type = gsub(pattern = "^-{1,}|-{1,}$", "", out_hh_type),
          out_hh_type = gsub(pattern = "-{1,}", ", ", out_hh_type))
 
 
 
-real.df.out$out_hh_type %>% unique()
-
-not.cols <- colnames(real.df.out) %>%
-  grep(pattern = "^py1|^hh1|^uy1", x = ., value = T)
-
-keep.cols <- colnames(real.df.out)[!colnames(real.df.out) %in%
-                        not.cols]
-
-final.out <- real.df.out[colnames(real.df.out) %in% keep.cols]
-
-# library(ggplot2)
-# 
-# ggplot() + 
-#   geom_hline(data = data.frame(y1 = c(17.5, 24.5)), 
-#              aes(yintercept = y1), color = "black", 
-#              linetype = 2232)+ 
-#   geom_boxplot(data = final.out, 
-#              aes(x = out_hh_type, group = out_hh_type,
-#                  y = min_age))+
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+
-#   labs(title = "Minimum Age (Distribution) by HH Type")+
-#   scale_y_continuous(breaks = seq(0,1000,by=10))
-# 
-# ggplot() + 
-#   geom_hline(data = data.frame(y1 = c(17.5, 24.5)), 
-#              aes(yintercept = y1), color = "black", 
-#              linetype = 2232)+ 
-#   geom_boxplot(data = final.out, 
-#                aes(x = out_hh_type, group = out_hh_type,
-#                    y = max_age))+
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+
-#   labs(title = "Maximum Age (Distribution) by HH Type")+
-#   scale_y_continuous(breaks = seq(0,1000,by=10))
-# 
-# 
-# ggplot() + 
-#   # geom_hline(data = data.frame(y1 = c(17.5, 24.5)), 
-#   #            aes(yintercept = y1), color = "black", 
-#   #            linetype = 2232)+ 
-#   geom_jitter(data = final.out, 
-#                aes(x = out_hh_type, group = out_hh_type,
-#                    y = n_vets))+
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+
-#   labs(title = "Number of Veterans in HH by HH_Type")+
-#   scale_y_continuous(breaks = seq(0,1000,by=1), 
-#                      minor_breaks = seq(0,1000,by=1))
-# 
-# 
-# 
-# out.join <- left_join(real.df, 
-#                       final.out) 
-# 
-# 
-# write_csv(x = final.out, 
-#           file = "householdID_level_MODULE_youthvethh.csv")
-# 
-# write_csv(x = out.join, 
-#           file = "personalID_level_MODULE_youthvethh.csv")
+# minimum colums
+real.df <- real.df[,c("PersonalID", "EnrollmentID", "HouseholdID", 
+                      "out_hh_type")]
