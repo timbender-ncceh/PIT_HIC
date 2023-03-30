ead5e2e4da662ed0b5f216a7fd3315d8fc1b61df
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-03-30 08:03:11 -0400

    Update pit_MODULE_chronicallyhomeless.R
    periodic

--- working_files/pit_MODULE_chronicallyhomeless.R
+++ working_files/pit_MODULE_chronicallyhomeless.R
@@ -1,2 +1,21 @@
-library(readr)
+library(openxlsx)
 
+
+# get filename----
+ch.input.name <- list.files(pattern = "^A003 -")
+
+# get sheets----
+ch.sheet.names <- loadWorkbook(file = ch.input.name) %>% names()
+
+# load data---
+ch.summary.df <- readWorkbook(ch.input.name, sheet = "Summary") %>%
+  as_tibble()
+ch.clientdetail.df <- readWorkbook(ch.input.name, sheet = "Client Detail") %>%
+  as_tibble()
+ch.disabilityDQerrors.df <- readWorkbook(ch.input.name,sheet = "Disability DQ Errors") %>%
+  as_tibble()
+
+# remove unneeded vars
+rm(ch.sheet.names, ch.input.name)
+
+# tidy----
