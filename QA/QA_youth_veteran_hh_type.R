dea750a7eeabd735fb09fc12cbf0a95bfcbcdcf7
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-05 11:53:00 -0400

    Update QA_youth_veteran_hh_type.R

--- QA/QA_youth_veteran_hh_type.R
+++ QA/QA_youth_veteran_hh_type.R
@@ -6,52 +6,78 @@ library(glue)
 rm(list=ls())
 
 ui <- fluidPage(h3("Person #1:"),
-  sidebarLayout(
-    sidebarPanel(
-      selectizeInput(inputId = 'vet_input01', 
-                     label = 'Veteran Status', 
-                     choices = c("Yes", "No", NA), 
-                     multiple = F),
-      shiny::sliderInput(inputId = "age_input01", 
-                         label = "Age", 
-                         min = 0, max = 80, step = 1, 
-                         value = 0)),
-    mainPanel(
-      verbatimTextOutput('values1')
-    )
-  ), 
-  h3("Person #2:"),
-  sidebarLayout(
-    sidebarPanel(
-      selectizeInput(inputId = 'vet_input02', 
-                     label = 'Veteran Status', 
-                     choices = c("Yes", "No", NA), 
-                     multiple = F),
-      shiny::sliderInput(inputId = "age_input02", 
-                         label = "Age", 
-                         min = 0, max = 80, step = 1, 
-                         value = 0)),
-    
-    mainPanel(label = "Output Results:",
-      verbatimTextOutput('values2')
-    )
-  ),
-  h3("Person #3:"),
-  sidebarLayout(
-    sidebarPanel(
-      selectizeInput(inputId = 'vet_input03', 
-                     label = 'Veteran Status', 
-                     choices = c("Yes", "No", NA), 
-                     multiple = F),
-      shiny::sliderInput(inputId = "age_input03", 
-                         label = "Age", 
-                         min = 0, max = 80, step = 1, 
-                         value = 0)),
-    mainPanel(
-      verbatimTextOutput('values3')
-    )
-  )#, 
-  #title = 'Options groups for select(ize) input'
+                sidebarLayout(
+                  sidebarPanel(
+                    shiny::sliderInput(inputId = "age_input01", 
+                                       label = "Age", 
+                                       min = 0, max = 80, step = 1, 
+                                       value = 0),
+                    selectizeInput(inputId = 'rel_input01', 
+                                   label = 'Relationship To HoH', 
+                                   choices = c(NA, 
+                                               "Self (head of household)", 
+                                               "Head of household's child", 
+                                               "Head of household's spouse or partner", 
+                                               "Head of household's other relation member", 
+                                               "Other: non-relation member"), 
+                                   multiple = F),
+                    selectizeInput(inputId = 'vet_input01', 
+                                   label = 'Veteran Status', 
+                                   choices = c("Yes", "No", NA), 
+                                   multiple = F)),
+                  mainPanel(
+                    verbatimTextOutput('values1')
+                  )
+                ), 
+                h3("Person #2:"),
+                sidebarLayout(
+                  sidebarPanel(
+                    shiny::sliderInput(inputId = "age_input02", 
+                                       label = "Age", 
+                                       min = 0, max = 80, step = 1, 
+                                       value = 0),
+                    selectizeInput(inputId = 'rel_input02', 
+                                   label = 'Relationship To HoH', 
+                                   choices = c(NA, 
+                                               "Self (head of household)", 
+                                               "Head of household's child", 
+                                               "Head of household's spouse or partner", 
+                                               "Head of household's other relation member", 
+                                               "Other: non-relation member"), 
+                                   multiple = F),
+                    selectizeInput(inputId = 'vet_input02', 
+                                   label = 'Veteran Status', 
+                                   choices = c("Yes", "No", NA), 
+                                   multiple = F)),
+                  mainPanel(label = "Output Results:",
+                            verbatimTextOutput('values2')
+                  )
+                ),
+                h3("Person #3:"),
+                sidebarLayout(
+                  sidebarPanel(
+                    shiny::sliderInput(inputId = "age_input03", 
+                                       label = "Age", 
+                                       min = 0, max = 80, step = 1, 
+                                       value = 0),
+                    selectizeInput(inputId = 'rel_input03', 
+                                   label = 'Relationship To HoH', 
+                                   choices = c(NA, 
+                                               "Self (head of household)", 
+                                               "Head of household's child", 
+                                               "Head of household's spouse or partner", 
+                                               "Head of household's other relation member", 
+                                               "Other: non-relation member"), 
+                                   multiple = F),
+                    selectizeInput(inputId = 'vet_input03', 
+                                   label = 'Veteran Status', 
+                                   choices = c("Yes", "No", NA), 
+                                   multiple = F)),
+                  mainPanel(
+                    verbatimTextOutput('values3')
+                  )
+                )#, 
+                #title = 'Options groups for select(ize) input'
 )
 
 server <- function(input,output,session){
@@ -90,14 +116,16 @@ server <- function(input,output,session){
   # Outputs here----
   output$values1 <- renderPrint({
     cat("ARGUMENT INPUTS:\n\n")
-    #paste("Ages:", )
+    cat(glue("Ages:\t\t{paste(c(input$age_input01,input$age_input02,input$age_input03),sep=\", \",collapse=\", \")}\n\n"))
+    cat(glue("Veteran:\t{paste(c(input$vet_input01,input$vet_input02,input$vet_input03),sep=\", \",collapse=\", \")}\n\n"))
+    cat(glue("Rel2HoH:\t{paste(c(input$rel_input01,input$rel_input02,input$rel_input03),sep=\", \",collapse=\", \")}\n\n"))
   })
   
   output$values2 <- renderPrint({
     cat(bold(inverse("LOGIC OUTPUTS:\n\n")))
     cat(glue("hh_vet():\t[1] {hh_vet(c(input$vet_input01,input$vet_input02,input$vet_input03))}\n\n"))
     cat(glue("hh_youth():\t[1] {hh_vet(c(input$age_input01,input$age_input02,input$age_input03))}\n\n"))
-   
+    
   })
 }
 
