bdc6b201b91f5247b05b88fa2be293d409586b58
Tim Bender <105810134+timbender-ncceh@users.noreply.github.com>
2023-04-07 13:01:48 -0400

    Update pit_MODULE_chronicallyhomeless_byHH.R

--- working_files/pit_MODULE_chronicallyhomeless_byHH.R
+++ working_files/pit_MODULE_chronicallyhomeless_byHH.R
@@ -23,28 +23,28 @@ temp.o2A <- output2A[,c("PersonalID", "HouseholdID", "EntryDate")] %>%
 CH_hhid <- inner_join(temp.o2A, 
              temp.csi)$HouseholdID %>% unique()
 
-CH.status.byHH <- temp.o2A[,c("PersonalID", "HouseholdID")] %>%
-  mutate(., 
-         pid_in_HH_wal1_CH = NA)
-
-CH.status.byHH$pid_in_HH_wal1_CH <- CH.status.byHH$HouseholdID %in% 
-  CH_hhid
-
-# remove duplicates
-CH.status.byHH <- CH.status.byHH[!duplicated(CH.status.byHH),]
-
-CH.status.byHH %>%
-  group_by(PersonalID) %>%
-  summarise(n_hhid = n_distinct(HouseholdID), 
-            n_hh.with.ch = sum(pid_in_HH_wal1_CH)) %>%
-  .[order(.$n_hh.with.ch, decreasing = T),] %>%
-  group_by(two.plus_hh = n_hhid >= 2) %>%
-  summarise(n_pid = n_distinct(PersonalID)) %>%
-  ungroup() %>%
-  mutate(., 
-         pct_pid = n_pid/sum(n_pid))
+# CH.status.byHH <- temp.o2A[,c("PersonalID", "HouseholdID")] %>%
+#   mutate(., 
+#          pid_in_HH_wal1_CH = NA)
+# 
+# CH.status.byHH$pid_in_HH_wal1_CH <- CH.status.byHH$HouseholdID %in% 
+#   CH_hhid
+# 
+# # remove duplicates
+# CH.status.byHH <- CH.status.byHH[!duplicated(CH.status.byHH),]
+# 
+# CH.status.byHH %>%
+#   group_by(PersonalID) %>%
+#   summarise(n_hhid = n_distinct(HouseholdID), 
+#             n_hh.with.ch = sum(pid_in_HH_wal1_CH)) %>%
+#   .[order(.$n_hh.with.ch, decreasing = T),] %>%
+#   group_by(two.plus_hh = n_hhid >= 2) %>%
+#   summarise(n_pid = n_distinct(PersonalID)) %>%
+#   ungroup() %>%
+#   mutate(., 
+#          pct_pid = n_pid/sum(n_pid))
 
 # cleanup----
-rm(CH_hhid, 
-   temp.o2A, 
+rm(temp.o2A,
+   # CH_hhid,
    temp.csi)
